0f6c525e2a0c1aab217395e7f00aa21f53b842f3
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-07-01 16:51:00 -0600

    highlight suggested tags from company description

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -4,8 +4,26 @@ var charCount = document.getElementById("charCount");
 charCount.innerHTML = 138 - prevWindow.innerHTML.length + " characters remaining";
 // var tweet = document.getElementById("tweet-container");
 
+function getCookies(input) {
+  var results = input.replace(/ /g, "").split(";");
+  var obj = {};
+  for (var i = 0; i < results.length; i++) {
+    var key = results[i].split("=")[0];
+    var value = results[i].split("=")[1];
+    obj[key] = value;
+  }
+  return obj;
+}
+var description = getCookies(document.cookie).description.split("%2C");
+
 var tags = "";
 for (var i = 0; i < results.length; i++) {
+  for (var j = 0; j < description.length; j++) {
+    if (results[i].id === description[j]) {
+      results[i].style.backgroundColor = "rgb(255, 255, 153)";
+      results[i].style.color = "black";
+    }
+  }
   results[i].onclick = function() {
     if (prevWindow.value.length + this.id.length > 138) {
       console.log(this.id.length);
@@ -16,6 +34,7 @@ for (var i = 0; i < results.length; i++) {
       prevWindow.innerHTML = prevWindow.value;
       charCount.innerHTML = 138 - prevWindow.value.length + " characters remaining";
       tags += this.id + ",";
+      this.style.backgroundColor = "silver";
       if (prevWindow.value.length >= 128) {
         charCount.style.color = "red";
         console.log(charCount);
